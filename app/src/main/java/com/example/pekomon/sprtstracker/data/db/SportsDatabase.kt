fb7c4eac86fe767f40f335aada23577432cc4db0
Pekomon <5183385+pekomon@users.noreply.github.com>
2021-01-02 19:40:33 +0200

    DB and TypeConverters

---
+++ app/src/main/java/com/example/pekomon/sprtstracker/data/db/SportsDatabase.kt
@@ -0,0 +1,18 @@
+package com.example.pekomon.sprtstracker.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.pekomon.sprtstracker.data.db.typeconverters.BitmapTypeConverter
+import com.example.pekomon.sprtstracker.data.entity.Run
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+
+@TypeConverters(BitmapTypeConverter::class)
+
+abstract class SportsDatabase : RoomDatabase() {
+    abstract fun runDao(): RunDao
+}
