1fd3481133c23cf53269832d82926e6577951c98
Pekomon <5183385+pekomon@users.noreply.github.com>
2021-01-29 22:57:38 +0200

    Show statistics

--- app/src/main/java/com/example/pekomon/sprtstracker/ui/fragment/StatisticsFragment.kt
+++ app/src/main/java/com/example/pekomon/sprtstracker/ui/fragment/StatisticsFragment.kt
@@ -1,15 +1,62 @@
 package com.example.pekomon.sprtstracker.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.pekomon.sprtstracker.R
+import com.example.pekomon.sprtstracker.databinding.FragmentStatisticsBinding
 import com.example.pekomon.sprtstracker.ui.viewmodel.MainViewModel
 import com.example.pekomon.sprtstracker.ui.viewmodel.StatisticsViewModel
+import com.example.pekomon.sprtstracker.utils.TimeUtils
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
     private val viewModel: StatisticsViewModel by viewModels()
+    private lateinit var binding: FragmentStatisticsBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentStatisticsBinding.bind(view)
+
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.totalTime.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                binding.tvTotalTime.text = TimeUtils.getFormattedTime(it, false)
+            }
+        })
+
+        viewModel.totalDistance.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                binding.tvTotalDistance.text = (round((it/1000) * 10f) / 10f).toString()
+            }
+        })
+
+        viewModel.totalDistance.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                binding.tvTotalDistance.text = (round((it/1000) * 10f) / 10f).toString()
+            }
+        })
+
+        viewModel.averageSpeed.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                binding.tvAverageSpeed.text = (round(it * 10f) / 10f).toString()
+            }
+        })
+
+        viewModel.totalCalories.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                binding.tvTotalCalories.text = it.toString()
+            }
+        })
+
+    }
 }
--- app/src/main/java/com/example/pekomon/sprtstracker/ui/viewmodel/StatisticsViewModel.kt
+++ app/src/main/java/com/example/pekomon/sprtstracker/ui/viewmodel/StatisticsViewModel.kt
@@ -4,8 +4,14 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import com.example.pekomon.sprtstracker.data.repository.MainRepository
 
-class StatisticsViewModel@ViewModelInject constructor(
+class StatisticsViewModel @ViewModelInject constructor(
     val repository: MainRepository
 ) : ViewModel()  {
 
+    val totalTime = repository.getTotalTimeInMillis()
+    val totalDistance = repository.getTotalDistance()
+    val totalCalories = repository.getTotalCalories()
+    val averageSpeed = repository.getAverageSpeed()
+
+    val runsByDate = repository.getAllRunsByDate()
 }
